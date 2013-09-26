e22a918e826710b54124aabc8a23794c0a22b180
Ruxton <ruxton@thefrontiergroup.com.au>
2013-09-26 15:48:30 +0800

    a little more portable deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ GITPATH="$CURRENTDIR/$PLUGINSLUG/" # this file should be in the base of your git
 
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/gmep-oembed-support/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="Ruxton" # your svn username
 
 
