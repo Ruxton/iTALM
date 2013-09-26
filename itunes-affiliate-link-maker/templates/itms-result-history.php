c75ef6d46c135da92d1b93fa728023f074a88093
Ruxton <ruxton@thefrontiergroup.com.au>
2013-09-26 13:21:45 +0800

    Minor formatting change

--- itunes-affiliate-link-maker/templates/itms-result-history.php
+++ itunes-affiliate-link-maker/templates/itms-result-history.php
@@ -9,25 +9,25 @@
 </head>
 <body>
 <table class="ita-results" id="ita-results-head" width="100%">
-    <tr>
-	    <th scope="col" width="482" ><?php _e('Name'); ?></th>
-            <th scope="col" ><?php _e('Date'); ?></th>
-    </tr>
+  <tr>
+    <th scope="col" width="482" ><?php _e('Name'); ?></th>
+    <th scope="col" ><?php _e('Date'); ?></th>
+  </tr>
 </table>
     <div id="ita-results-scroll">
 <table class="ita-results" id="ita-results-body" width="100%">
 <?php
-    $i = 0;
-	$ita_linkImage = ita::setting('ita-linkimage');
-    foreach($queryRes as $result) {
-    ?>
-            <tr>
-                    <td width="70%"<?php echo( $i == 0 ? ' class="odd"' : '' ); ?>><a href="<?php echo($result->linkUrl); ?>" onClick="italm_linkIt('<?php echo($result->linkName); ?>',this.href,'<?php echo($ita_linkImage); ?>');return false;"><?php echo($result->linkName); ?></a></td>
-                    <td width="30%"<?php echo( $i == 0 ? ' class="odd"' : '' ); ?>><?php echo date("F j, Y, g:i a O", $result->updateTime ); ?></td>
-            </tr>
-    <?php
-            $i == 0 ? $i = 1 : $i = 0;
-    }
+  $i = 0;
+  $ita_linkImage = ita::setting('ita-linkimage');
+  foreach($queryRes as $result) {
+  ?>
+    <tr>
+      <td width="70%"<?php echo( $i == 0 ? ' class="odd"' : '' ); ?>><a href="<?php echo($result->linkUrl); ?>" onClick="italm_linkIt('<?php echo($result->linkName); ?>',this.href,'<?php echo($ita_linkImage); ?>');return false;"><?php echo($result->linkName); ?></a></td>
+      <td width="30%"<?php echo( $i == 0 ? ' class="odd"' : '' ); ?>><?php echo date("F j, Y, g:i a O", $result->updateTime ); ?></td>
+    </tr>
+  <?php
+    $i == 0 ? $i = 1 : $i = 0;
+  }
 ?>
 </table>
     </div>
